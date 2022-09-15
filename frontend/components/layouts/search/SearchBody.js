ec09e13487384e72176591f4cc69c22771d0811f
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-09-15 15:35:03 +0900

    전적검색 아이템 추가

--- frontend/components/layouts/search/SearchBody.js
+++ frontend/components/layouts/search/SearchBody.js
@@ -1,13 +1,11 @@
 import styles from "./SearchBody.module.css";
 
 const champion =[
-    {id :1, cham : "Aatrox", date : "2022-09-08", result : true, spell1 : "flash", spell2 : "teleport"},
-    {id :2, cham : "Gwen", date : "2022-09-07", result : true, spell1 : "ignite", spell2 : "teleport"},
-    {id :3, cham : "Vladimir", date : "2022-09-06", result : false, spell1 : "ignite", spell2 : "ghost"}
+    {cham : "Aatrox", date : "2022-09-08", result : true, spell1 : "flash", spell2 : "teleport", item1:"", item2:"",item3:"",item4:"", item5:"",item6:""},
+    {cham : "Gwen", date : "2022-09-07", result : true, spell1 : "ignite", spell2 : "teleport",item1:"", item2:"",item3:"",item4:"", item5:"",item6:""},
+    {cham : "Vladimir", date : "2022-09-06", result : false, spell1 : "ignite", spell2 : "ghost",item1:"", item2:"",item3:"",item4:"", item5:"",item6:""}
   ]
 export default function SearchBody() {
-  
-
   return (
     <>
         <div className={styles.result}>
@@ -15,10 +13,11 @@ export default function SearchBody() {
                 let championImg = `/champion/tiles/${cham.cham}_0.jpg`
                 let spell1Img = `/spell/${cham.spell1}.png`;
                 let spell2Img = `/spell/${cham.spell2}.png`;
+                
                 return(
                     <div key={idx}>
                         <div className={cham.result === true ? styles.win : styles.lose}>
-                                <label className={styles.label}>{cham.date}</label>
+                                <label className={styles.label}>{cham.date}<br></br>{cham.result===true? "win":"lose"}</label>
                                 <div>
                                     <img src={championImg} width={75} height={75}></img>
                                 </div>
@@ -27,10 +26,15 @@ export default function SearchBody() {
                                     <img className={styles.spell} src={spell2Img} width={35} height={35}></img>
                                 </div>
                                 <div>
+                                    <img className={styles.item} src={`${cham.item1}`=== "" ? "/item/noItem.png":`"/item/${cham.item1}.png"`}></img>
+                                    <img className={styles.item} src={`${cham.item2}`=== "" ? "/item/noItem.png":`"/item/${cham.item2}.png"`}></img>
+                                    <img className={styles.item} src={`${cham.item3}`=== "" ? "/item/noItem.png":`"/item/${cham.item3}.png"`}></img>
+                                    <img className={styles.item} src={`${cham.item4}`=== "" ? "/item/noItem.png":`"/item/${cham.item4}.png"`}></img>
+                                    <img className={styles.item} src={`${cham.item5}`=== "" ? "/item/noItem.png":`"/item/${cham.item5}.png"`}></img>
+                                    <img className={styles.item} src={`${cham.item6}`=== "" ? "/item/noItem.png":`"/item/${cham.item6}.png"`}></img>
                                 </div>
-                            </div>
+                        </div>
                     </div>
-
                 );
             })
             }
