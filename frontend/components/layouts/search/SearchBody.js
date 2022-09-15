fb56dd63c86c5b6ef1071f5e67662730d3f42b26
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-09-15 15:07:00 +0900

    전적검색 페이지 검색결과 추가

--- frontend/components/layouts/search/SearchBody.js
+++ frontend/components/layouts/search/SearchBody.js
@@ -1,12 +1,40 @@
 import styles from "./SearchBody.module.css";
-import Image from "next/image";
+
+const champion =[
+    {id :1, cham : "Aatrox", date : "2022-09-08", result : true, spell1 : "flash", spell2 : "teleport"},
+    {id :2, cham : "Gwen", date : "2022-09-07", result : true, spell1 : "ignite", spell2 : "teleport"},
+    {id :3, cham : "Vladimir", date : "2022-09-06", result : false, spell1 : "ignite", spell2 : "ghost"}
+  ]
 export default function SearchBody() {
+  
+
   return (
     <>
-        <div className={styles.icon}>
-            <Image src={icon} layout="intrinsic" width={100} height={100}></Image>
-            <label className={styles.label}> summonerId </label>
-        </div>
+        <div className={styles.result}>
+            {champion.map((cham, idx)=>{
+                let championImg = `/champion/tiles/${cham.cham}_0.jpg`
+                let spell1Img = `/spell/${cham.spell1}.png`;
+                let spell2Img = `/spell/${cham.spell2}.png`;
+                return(
+                    <div key={idx}>
+                        <div className={cham.result === true ? styles.win : styles.lose}>
+                                <label className={styles.label}>{cham.date}</label>
+                                <div>
+                                    <img src={championImg} width={75} height={75}></img>
+                                </div>
+                                <div>
+                                    <img className={styles.spell} src={spell1Img} width={35} height={35}></img>
+                                    <img className={styles.spell} src={spell2Img} width={35} height={35}></img>
+                                </div>
+                                <div>
+                                </div>
+                            </div>
+                    </div>
+
+                );
+            })
+            }
+            </div>
     </>
   );
 }
--- frontend/components/layouts/search/SearchTop.js
+++ frontend/components/layouts/search/SearchTop.js
@@ -1,14 +1,12 @@
 import styles from "./SearchTop.module.css";
-import Image from "next/image";
 export default function SearchTop() {
   const icon = "/spell/barrier.png";
   return (
     <>
         <div className={styles.icon}>
-            <Image src={icon} layout="intrinsic" width={100} height={100}></Image>
+            <img src="/spell/barrier.png" width={100} height={100}></img>
             <label className={styles.label}> summonerId </label>
         </div>
-
     </>
   );
 }
