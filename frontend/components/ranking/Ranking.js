a1a59f5aaa6fe7bc673b5dcd501993bc02b549c7
최영선 <vnddns26@naver.com>
2022-09-15 14:35:57 +0900

    feature/Ranking scroll

--- frontend/components/ranking/Ranking.js
+++ frontend/components/ranking/Ranking.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "./Ranking.module.css";
 
 export default function Ranking() {
@@ -1238,8 +1238,15 @@ export default function Ranking() {
   ]);
   const [page, setPage] = useState(1);
   const [lenArr, setLenArr] = useState([]);
+  const scrollRef = useRef();
+  const scrollTop = function () {
+    scrollRef.current.scrollIntoView({
+      block: "start",
+      inline: "nearest",
+    });
+  };
   useEffect(() => {
-    setLenArr([...Array(parseInt(user.length / 10) + 1).keys()]);
+    setLenArr([...Array(parseInt(user.length / 100) + 1).keys()]);
   }, [page]);
   return (
     <div className={styles.main}>
@@ -1247,44 +1254,59 @@ export default function Ranking() {
         <h1>Ranking</h1>
       </div>
       <div className={styles.container}>
-        <div className={styles.articles}>
-          <div className={styles.att}>
-            <div>Rank</div>
-            <div>Summoner</div>
-            <div>Tier</div>
-            <div>LP</div>
-            <div>Most Champion</div>
-            <div>Win Rate</div>
-          </div>
-          {user
-            .filter((num, idx) => {
-              return parseInt(idx / 10) + 1 === page ? num : null;
-            })
-            .map((user, idx) => {
-              return (
-                <div key={idx} className={styles.article}>
-                  <div>{user.rank}</div>
-                  <div>{user.summoner}</div>
-                  <div>{user.tier}</div>
-                  <div>{user.lp}</div>
-                  <div className={styles.imgs}>
-                    <img
-                      src={`/champion/tiles/${user.mostChampion[0]}_0.jpg`}
-                      className={styles.img}
-                    ></img>
-                    <img
-                      src={`/champion/tiles/${user.mostChampion[1]}_0.jpg`}
-                      className={styles.img}
-                    ></img>
-                    <img
-                      src={`/champion/tiles/${user.mostChampion[2]}_0.jpg`}
-                      className={styles.img}
-                    ></img>
+        <div className={styles.att}>
+          <div>Rank</div>
+          <div>Summoner</div>
+          <div>Tier</div>
+          <div>LP</div>
+          <div>Most Champion</div>
+          <div>Win Rate</div>
+        </div>
+        <div className={styles.border}>
+          <div className={styles.articles}>
+            {/* <div className="styles.articleContainer"> */}
+            {user
+              .filter((num, idx) => {
+                return parseInt(idx / 100) + 1 === page ? num : null;
+              })
+              .map((user, idx) => {
+                return (
+                  <div
+                    key={idx}
+                    className={styles.article}
+                    ref={idx === 0 ? scrollRef : null}
+                  >
+                    <div>{user.rank}</div>
+                    <div>{user.summoner}</div>
+                    <div>{user.tier}</div>
+                    <div>{user.lp}</div>
+                    <div className={styles.imgs}>
+                      <img
+                        src={`/champion/tiles/${user.mostChampion[0]}_0.jpg`}
+                        className={styles.img}
+                      ></img>
+                      <img
+                        src={`/champion/tiles/${user.mostChampion[1]}_0.jpg`}
+                        className={styles.img}
+                      ></img>
+                      <img
+                        src={`/champion/tiles/${user.mostChampion[2]}_0.jpg`}
+                        className={styles.img}
+                      ></img>
+                    </div>
+                    <div>
+                      <progress
+                        className={styles.progress}
+                        value={user.winRate}
+                        min="0"
+                        max="100"
+                      ></progress>
+                    </div>
                   </div>
-                  <div>{user.winRate}</div>
-                </div>
-              );
-            })}
+                );
+              })}
+            {/* </div> */}
+          </div>
         </div>
       </div>
       <div className={styles.buttons}>
@@ -1295,6 +1317,7 @@ export default function Ranking() {
                 className={styles.button}
                 onClick={(e) => {
                   setPage(Number(e.target.innerText));
+                  scrollTop();
                 }}
               >
                 {num + 1}
