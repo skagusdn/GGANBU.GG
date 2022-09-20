c7e41e5c939bc755be2b38e2c2d0e1d053e6633e
JMBFLY <parkmoochang@gmail.com>
2022-09-20 17:04:10 +0900

    skill img import

--- frontend/components/detailChampion/Detailchampion.js
+++ frontend/components/detailChampion/Detailchampion.js
@@ -1,8 +1,34 @@
 import { useRouter } from "next/router";
+import styles from "./Detailchampion.module.css";
+import championList from "../../utils/champion";
+import { useEffect } from "react";
 
 export default function DetailChampion() {
   const router = useRouter();
   const { id } = router.query;
 
-  return <p>Post: {id}</p>;
+  const clist = championList();
+
+  return (
+    <div>
+      <div className={styles.container}>
+        <div className={styles.profile}>
+          <img src={`/champion/tiles/${id}_0.jpg`}></img>
+          <span>
+            {id ? clist.find((element) => element.en === id).ko : null}
+          </span>
+          <div>
+            <img></img>
+            <img></img>
+            <img></img>
+            <img></img>
+            <img></img>
+          </div>
+        </div>
+        <div className={styles.rune}></div>
+        <div className={styles.skill}></div>
+        <div className={styles.item}></div>
+      </div>
+    </div>
+  );
 }
