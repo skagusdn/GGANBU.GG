3d92bc238d6b52e103461fdf2324a47fce701fd9
CYoungSun <vnddns26@naver.com>
2022-09-23 16:12:40 +0900

    feature/playername

--- frontend/components/multi/Multi.js
+++ frontend/components/multi/Multi.js
@@ -4,7 +4,14 @@ import Search from "../search/Search";
 import styles from "./Multi.module.css";
 
 export default function Multi() {
-  const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState(true);
+  const [players, setPlayers] = useState([
+    "noPlayer",
+    "noPlayer",
+    "noPlayer",
+    "noPlayer",
+    "noPlayer",
+  ]);
   function clickButton() {
     setModal(!modal);
   }
@@ -13,7 +20,7 @@ export default function Multi() {
     const newPlayers = players.map((player) => {
       return player.substring(0, player.length - 14);
     });
-    console.log(newPlayers);
+    setPlayers(newPlayers);
   }
   return (
     <div className={styles.main}>
@@ -33,31 +40,31 @@ export default function Multi() {
           검색
         </button>
         <div className={styles.top}>
-          <h3>TOP</h3>
+          <h3>{players[0]}</h3>
           <div className={styles.img}></div>
           <div className={styles.champ}></div>
           <div className={styles.search}></div>
         </div>
         <div className={styles.jug}>
-          <h3>JUG</h3>
+          <h3>{players[1]}</h3>
           <div className={styles.img}></div>
           <div className={styles.champ}></div>
           <div className={styles.search}></div>
         </div>
         <div className={styles.mid}>
-          <h3>MID</h3>
+          <h3>{players[2]}</h3>
           <div className={styles.img}></div>
           <div className={styles.champ}></div>
           <div className={styles.search}></div>
         </div>
         <div className={styles.bot}>
-          <h3>BOT</h3>
+          <h3>{players[3]}</h3>
           <div className={styles.img}></div>
           <div className={styles.champ}></div>
           <div className={styles.search}></div>
         </div>
         <div className={styles.sup}>
-          <h3>SUP</h3>
+          <h3>{players[4]}</h3>
           <div className={styles.img}></div>
           <div className={styles.champ}></div>
           <div className={styles.search}></div>
