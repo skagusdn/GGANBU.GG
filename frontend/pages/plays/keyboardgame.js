335b9c71dba6f17f4b99df81394ecd78acfa01be
최영선 <vnddns26@naver.com>
2022-09-28 10:52:59 +0900

    feture/game

--- frontend/pages/plays/keyboardgame.js
+++ frontend/pages/plays/keyboardgame.js
@@ -1,27 +1,63 @@
 import React, { useEffect, useRef, useState } from "react";
-import styles from "../styles/Home.module.css";
 const Keyboard = () => {
-  let timertime = 10;
+  let timertime = 11;
   let pressstop = false;
-
+  let playerTime = 0;
   const [result, setResult] = useState("");
   const [keyboardinput, Setkeyboardinput] = useState("");
-
+  const [score, setScore] = useState(0);
+  const [start, setStart] = useState(0);
+  const difficulty = [100, 80, 60, 40, 20];
+  const [stage, setStage] = useState(0);
   const showtime = useRef();
-
+  console.log(stage);
   useEffect(() => {
     document.addEventListener("keydown", detectKey, true);
-  }, []);
+  }, [stage]);
 
   const detectKey = (e) => {
     console.log("clicked key: ", e.key);
-    if (e.key === "b") {
-      startTimer(timertime);
-    }
     if (e.key === "a") {
       pressstop = true;
     }
   };
+
+  function startGame(stage) {
+    console.log(stage);
+    const st = setInterval(function () {
+      setStart((start) => {
+        if (start === 5) {
+          startTime(stage);
+          clearInterval(st);
+          setStart(0);
+        }
+        return start + 1;
+      });
+    }, 1000);
+  }
+
+  function startTime(stage) {
+    console.log(stage);
+    const time = setInterval(function () {
+      playerTime++;
+      if (pressstop === true) {
+        setScore(playerTime);
+        console.log(stage);
+        if (stage >= 4) {
+          setStage("CLEAR");
+        } else if (playerTime <= difficulty[stage]) {
+          setStage((stage) => {
+            return stage + 1;
+          });
+        } else {
+          setStage("GAMEOVER");
+        }
+        clearInterval(time);
+        playerTime = 0;
+        pressstop = false;
+      }
+    }, 10);
+  }
 
   function startTimer(timertime) {
     var hour, min, sec;
@@ -56,13 +92,25 @@ const Keyboard = () => {
       } else {
         Setkeyboardinput(`${th} : ${tm} : ${ts}`);
       }
-    }, 100);
+    }, 1000);
   }
 
   return (
-    <div className={styles.container}>
-      <span>{result}</span>
-      <span ref={showtime}>{keyboardinput}</span>
+    <div>
+      {/* <div>{result}</div> */}
+      {/* <span ref={showtime}>{keyboardinput}</span> */}
+      <div>{start}</div>
+      <button
+        onClick={() => {
+          startGame(stage);
+        }}
+      >
+        Start
+      </button>
+      {stage === "CLEAR" && <div>CLEAR</div>}
+      {stage === "GAMEOVER" && stage !== "CLEAR" && <div>GAMEOVER</div>}
+      {stage !== "GAMEOVER" && stage !== "CLEAR" && <div>Stage{stage + 1}</div>}
+      {score}
     </div>
   );
 };
