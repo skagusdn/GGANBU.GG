a32b0bc4a031929e1dd91b58e224a41e561fb0c3
CYoungSun <vnddns26@naver.com>
2022-10-04 11:12:25 +0900

    change file

--- frontend/components/skillQuiz/skillQuiz.js
+++ frontend/components/skillQuiz/skillQuiz.js
@@ -1,10 +1,12 @@
 import { useEffect, useRef, useState } from "react";
-import styles from "./SkillQuiz.module.css";
+import styles from "./skillQuiz.module.css";
 import championList from "../../utils/champion";
 
 export default function SkillQuiz({ setMode }) {
   const clist = championList();
-  const [randomChampion, setRandomChampion] = useState(Math.floor(Math.random() * 161));
+  const [randomChampion, setRandomChampion] = useState(
+    Math.floor(Math.random() * 161)
+  );
   const [randomSkill, setRandomSkill] = useState(Math.floor(Math.random() * 5));
   const [value, setValue] = useState("");
   const [score, setScore] = useState(0);
@@ -15,30 +17,37 @@ export default function SkillQuiz({ setMode }) {
 
   useEffect(() => {
     setValue("");
-  }, [randomChampion, randomSkill])
+  }, [randomChampion, randomSkill]);
 
   return (
     <>
-      {gameStart === false &&
+      {gameStart === false && (
         <div className={styles.container}>
           <div className={styles.quiz}>
             1분동안 제시된 스킬을 가진 챔피언을 맞추세요!
           </div>
-          <button className={styles.btn} onClick={() => {
-            setGameStart(true);
-            intervalId.current = setInterval(() => setTimer((timer) => timer - 1), 1000);
-          }}>
+          <button
+            className={styles.btn}
+            onClick={() => {
+              setGameStart(true);
+              intervalId.current = setInterval(
+                () => setTimer((timer) => timer - 1),
+                1000
+              );
+            }}
+          >
             게임 시작
           </button>
         </div>
-      }
+      )}
 
-      {gameStart === true && timer >= 0 &&
+      {gameStart === true && timer >= 0 && (
         <>
-
           <div className={styles.container}>
             <div className={styles.timer}>{timer}</div>
-            <div className={styles.quiz}>해당 스킬을 가진 챔피언을 입력하시오.</div>
+            <div className={styles.quiz}>
+              해당 스킬을 가진 챔피언을 입력하시오.
+            </div>
             {randomSkill === 0 ? (
               <img
                 src={`/passive/${clist[randomChampion].passive}.png`}
@@ -71,7 +80,8 @@ export default function SkillQuiz({ setMode }) {
               />
             )}
             <div className={styles.textandi}>
-              <input className={styles.text}
+              <input
+                className={styles.text}
                 placeholder="챔피언 이름"
                 value={value}
                 required={true}
@@ -79,65 +89,80 @@ export default function SkillQuiz({ setMode }) {
                   setValue(event.target.value);
                 }}
                 onKeyDown={(event) => {
-                  if (event.key === 'Enter') {
+                  if (event.key === "Enter") {
                     if (value === clist[randomChampion].ko) {
                       setInfo("맞았습니다!");
                       setScore((score) => score + 1);
                     } else {
-                      setInfo("틀렸습니다!")
+                      setInfo("틀렸습니다!");
                     }
                     setRandomChampion(() => {
                       return Math.floor(Math.random() * 161);
                     });
                     setRandomSkill(() => {
                       return Math.floor(Math.random() * 5);
-                    })
+                    });
                   }
                 }}
               ></input>
               <i></i>
             </div>
-            <button className={styles.btn} onClick={() => {
-              if (value === clist[randomChampion].ko) {
-                setInfo("맞았습니다!");
-                setScore((score) => score + 1);
-              } else {
-                setInfo("틀렸습니다!")
-              }
-              setRandomChampion(() => {
-                return Math.floor(Math.random() * 161);
-              });
-              setRandomSkill(() => {
-                return Math.floor(Math.random() * 5);
-              })
-            }}>
+            <button
+              className={styles.btn}
+              onClick={() => {
+                if (value === clist[randomChampion].ko) {
+                  setInfo("맞았습니다!");
+                  setScore((score) => score + 1);
+                } else {
+                  setInfo("틀렸습니다!");
+                }
+                setRandomChampion(() => {
+                  return Math.floor(Math.random() * 161);
+                });
+                setRandomSkill(() => {
+                  return Math.floor(Math.random() * 5);
+                });
+              }}
+            >
               입력
             </button>
 
-            <div className={info === "맞았습니다!" ? styles.answer : styles.wrong}>{info}</div>
+            <div
+              className={info === "맞았습니다!" ? styles.answer : styles.wrong}
+            >
+              {info}
+            </div>
             <div className={styles.score}>맞힌 갯수 : {score}</div>
           </div>
         </>
-      }
-      {gameStart === true && timer < 0 &&
-        <div className={styles.container} >
+      )}
+      {gameStart === true && timer < 0 && (
+        <div className={styles.container}>
           <span className={styles.showcount}>{score}개를 맞추셨어요!</span>
           <span className={styles.msg}>{score < 5 && " 좀더 분발하세요!"}</span>
-          <span className={styles.msg}>{score < 10 && score >= 5 && " 롤을 해보긴 하셨군요."}</span>
-          <span className={styles.msg}>{score < 15 && score >= 10 && " 상당한 실력의 소유자시군요!"}</span>
-          <span className={styles.msg}>{score >= 15 && " 롤을 그만하셔야 될 것 같습니다!"}</span>
-          <button className={styles.btn} onClick={() => {
-            setGameStart(false);
-            setScore(0);
-            setTimer(60);
-            clearInterval(intervalId.current);
-            setInfo("");
-          }}>
+          <span className={styles.msg}>
+            {score < 10 && score >= 5 && " 롤을 해보긴 하셨군요."}
+          </span>
+          <span className={styles.msg}>
+            {score < 15 && score >= 10 && " 상당한 실력의 소유자시군요!"}
+          </span>
+          <span className={styles.msg}>
+            {score >= 15 && " 롤을 그만하셔야 될 것 같습니다!"}
+          </span>
+          <button
+            className={styles.btn}
+            onClick={() => {
+              setGameStart(false);
+              setScore(0);
+              setTimer(60);
+              clearInterval(intervalId.current);
+              setInfo("");
+            }}
+          >
             메인화면으로
           </button>
         </div>
-
-      }
+      )}
     </>
   );
 }
