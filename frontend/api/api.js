6166c6d9f8eeae072bb26f88e8d6bf4f479be98d
CYoungSun <vnddns26@naver.com>
2022-10-05 11:04:13 +0900

    modify host

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,13 +1,13 @@
 //const HOST = "http://localhost:8080/"
-const HOST = "http://j7c204.p.ssafy.io/"
-const WORLDCUP = "worldcup/"
+const HOST = "http://j7c204.p.ssafy.io/api/";
+const WORLDCUP = "worldcup/";
 
-const worldcup ={
-    getAllChampion : () => HOST + WORLDCUP +"/",
-    getChampionByName : () => HOST + WORLDCUP +"/",
-    updateGoldMedal : () => HOST + WORLDCUP + "goldmedal/",
-    getGoldMedalCount : () => HOST +WORLDCUP +"goldmedalcount/",
-    getWinRate : () => HOST + WORLDCUP + "winrate/"
-}
+const worldcup = {
+  getAllChampion: () => HOST + WORLDCUP + "/",
+  getChampionByName: () => HOST + WORLDCUP + "/",
+  updateGoldMedal: () => HOST + WORLDCUP + "goldmedal/",
+  getGoldMedalCount: () => HOST + WORLDCUP + "goldmedalcount/",
+  getWinRate: () => HOST + WORLDCUP + "winrate/",
+};
 
-export {worldcup};
+export { worldcup };
